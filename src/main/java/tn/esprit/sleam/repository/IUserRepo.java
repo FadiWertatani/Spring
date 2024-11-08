88b51273bc06f782c2d1215c871e0e5a5af060fe
Fadi <91259013+FadiWertatani@users.noreply.github.com>
2024-11-08 15:43:16 +0100

    f

--- src/main/java/tn/esprit/sleam/repository/IUserRepo.java
+++ src/main/java/tn/esprit/sleam/repository/IUserRepo.java
@@ -1,9 +1,23 @@
 package tn.esprit.sleam.repository;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tn.esprit.sleam.entity.User;
 
+import java.util.List;
+
 @Repository
 public interface IUserRepo extends JpaRepository<User, Long> {
+    //1ér methode
+    //récuperer la liste des user par cour
+    List<User> findUsersByCoursAdministrationNomCours(String nomCours);
+
+    //2éme methode
+    //JPQL: JAVA Persistance Query Language
+    @Query("select  user from User user join user.coursAdministration coursAdmin where coursAdmin.nomCours=:nom")
+    List<User> usersParNomCours(@Param("nom") String nom);
+
 }
--- src/main/java/tn/esprit/sleam/service/UserServiceImpl.java
+++ src/main/java/tn/esprit/sleam/service/UserServiceImpl.java
@@ -52,4 +52,5 @@ public class UserServiceImpl implements IUserService {
         userRepo.save(user);
         return user;
     }
+
 }
